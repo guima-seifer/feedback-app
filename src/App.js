bc23125ae64b84765dd23cfcd91198a8fef5c094
guima-seifer <fernando.b.v.guimaraes@gmail.com>
2022-08-28 23:31:43 +0100

    feedbackForm

--- src/App.js
+++ src/App.js
@@ -2,24 +2,28 @@ import Header from './components/Header'
 import FeedbackData from './data/FeedbackData'
 import { useState } from 'react'
 import FeedbackList from './components/FeedbackList'
+import FeedbackStats from './components/FeedbackStats'
+import FeedbackForm from './components/FeedbackForm'
 
 function App() {
-    const [feedback, setFeedback] = useState(FeedbackData)
-    const deleteFeedback = (id) => {
-        if(window.confirm('Are you sure you want to delete?')){
-            //devolve array sem o id passado
-            setFeedback(feedback.filter((item) => item.id !== id))
-        }
+  const [feedback, setFeedback] = useState(FeedbackData)
+  const deleteFeedback = (id) => {
+    if (window.confirm('Are you sure you want to delete?')) {
+      //devolve array sem o id passado
+      setFeedback(feedback.filter((item) => item.id !== id))
     }
-    
-    //wrap with a div, because only one element can be returned
-    return (
-        <>
-        <Header />
-        <div className='container'>
-        <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
-        </div>
-        </>
-    )
-} 
+  }
+
+  //wrap with a div, because only one element can be returned
+  return (
+    <>
+      <Header />
+      <div className='container'>
+        <FeedbackForm />
+        <FeedbackStats feedback={feedback} />
+        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+      </div>
+    </>
+  )
+}
 export default App

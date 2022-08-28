496e54fb91d484630e5e964f3f3483687eef983e
guima-seifer <fernando.b.v.guimaraes@gmail.com>
2022-08-28 17:19:11 +0100

    conditionals steps

--- src/App.js
+++ src/App.js
@@ -1,6 +1,30 @@
 function App() {
+    const title = 'Feedback app'
+    const body = ''
+    const comments = [
+        {id: 1, text: 'Comment one'},
+        {id: 2, text: 'Comment two'},
+        {id: 3, text: 'Comment three'},
+        ]
+
+    //wrap with a div, because only one element can be returned
     return (
-    <h1>Hello from the component</h1>
+        <div className='container'>
+        <h1>{title.toUpperCase()}</h1>
+        <p>{body}</p>
+
+        <div className='comments'>
+        <h3>Comments ({comments.length})</h3>
+        <ul>
+            {comments.map((comment, index) => (
+                <li key={index}>{comment.text}</li>
+            ))}
+        </ul>
+        <p></p>
+
+        </div>
+
+        </div>
     )
 }
 export default App

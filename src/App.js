48d961e1b9fc50d0869a55bc678d4882ed0bf88b
guima-seifer <fernando.b.v.guimaraes@gmail.com>
2022-08-28 19:55:04 +0100

    feedbackStats

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ import FeedbackList from './components/FeedbackList'
 function App() {
     const [feedback, setFeedback] = useState(FeedbackData)
     const deleteFeedback = (id) => {
-
+        if(window.confirm('Are you sure you want to delete?')){
+            //devolve array sem o id passado
+            setFeedback(feedback.filter((item) => item.id !== id))
+        }
     }
     
     //wrap with a div, because only one element can be returned
